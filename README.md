dc3b315ebee70bb463c769ec189050f793c03a03
Krzysztof-Wolinski <wolin51@gmail.com>
2021-08-21 12:39:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ksiegarnia-4pi6c1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ksiegarnia-4pi6c1)
---
+++ index.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter'
