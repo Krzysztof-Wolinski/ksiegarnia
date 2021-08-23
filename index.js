5a8ffc3d13729f349d7ffc7ea418184d830ed512
Wolin5 <you@example.com>
2021-08-23 15:43:49 +0200

    update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-import './style.css';
+// import './style.css';
 
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter'
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter';
+
+fetch(API_URL, {
+    method : 'GET'
+  }).then((resp) => {
+        if (resp.ok){
+            return resp.text()
+        } else {
+            throw new Error("błąd sieci");
+        }
+    })
+    .catch((err) => {
+        console.log("błąd", err);
+    });
